dea08b328f387a21c95fd30601999c92220c504f
Oli <dev.olivercarlos@gmail.com>
2020-03-11 21:20:47 +0800

    Add store endpoint for player.

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 Route::group(['prefix' => 'v1/players'], function () {
-    Route::get('', 'API\V1\PlayerController@index');
-    Route::get('{player}', 'API\V1\PlayerController@show');
-    Route::get('/guzzle/test', 'API\V1\PlayerController@guzzle');
+    Route::get('', 'API\V1\PlayerController@index')->name('player.index');
+    Route::get('{player}', 'API\V1\PlayerController@show')->name('player.show');
+    Route::post('/create', 'API\V1\PlayerController@store')->name('player.show')
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
